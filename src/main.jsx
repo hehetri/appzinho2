b2bd1da48db78f1e0e679691641665e96fb5ec6e
Lucas Moreira <hehetri@gmail.com>
2025-04-13 23:32:26 +0100

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import DeliverooTracker from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DeliverooTracker />
+  </React.StrictMode>
+);
